e5915ba74a4381ae5814eab350a9325978674613
PhanHoangLocAG <phanhoangloc2017@gmail.com>
2022-01-06 14:37:04 +0700

    find

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-var arr = [1,2,3,4,5];
+var arr = [1,3,7,4,5,2,6,-5];
 
-var arr1 = arr.map(item => item * 2);
+var arr1 = arr.find(item => item == 2);
+var arr2 = arr.find(item => item > 4);
+var arr3= arr.find(item => item <0 );
 console.log(arr1);
-console.log(arr);
-// map function is map all item of array with new value.
+console.log(arr2);
+console.log(arr3);
+// find function is use search item with condition,return item valid with condition,return value not array
